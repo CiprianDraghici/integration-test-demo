386562baf6d92039f8b50606c6248e0bd531cd63
cipriandraghici <cipriandraghici93@gmail.com>
2023-10-12 17:51:41 +0300

    cleanup

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -2,16 +2,10 @@
 
 set -e # Exit with nonzero exit code if anything fails
 
-echo "Installing git..."
-sudo apt-get install jq
-#sudo apt-get install git-all
-
 echo "Installing yarn..."
 npm install --global yarn
 
-echo "Cloning mx-template-dapp..."
 git clone https://github.com/multiversx/mx-template-dapp.git
-
 
 echo "cd mx-template-dapp..."
 cd mx-template-dapp
@@ -21,17 +15,16 @@ git checkout integration
 
 # Define the package name and the new version number
 PACKAGE_NAME="@multiversx/sdk-dapp"
-#NEW_VERSION="git://github.com/multiversx/mx-sdk-dapp.git#development"
 NEW_VERSION="multiversx/mx-sdk-dapp.git#development"
 
 # Use jq to update the sdk-dapp package version in package.json
 jq --arg new_version "$NEW_VERSION" '.dependencies |= . + {("'"$PACKAGE_NAME"'"): $new_version}' package.json > temp.json && mv temp.json package.json
-echo "$PACKAGE_NAME version has been updated to $NEW_VERSION"
+echo "$PACKAGE_NAME version has been updated to $NEW_VERSION in package.json"
 
 # Display the new version of the package.json file
 cat package.json
 
-echo "Installing dependencies mx-template-dapp..."
+echo "Installing dependencies for mx-template-dapp..."
 yarn install
 echo "Building mx-template-dapp..."
 yarn build:devnet
