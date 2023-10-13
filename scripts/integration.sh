c4b340336a6c47bc31da5de74fc21fc3384ac30c
cipriandraghici <cipriandraghici93@gmail.com>
2023-10-13 10:46:52 +0300

    reworked script

--- scripts/integration.sh
+++ scripts/integration.sh
@@ -4,28 +4,34 @@ set -e # Exit with nonzero exit code if anything fails
 
 echo "Installing yarn..."
 npm install --global yarn
+echo "Installing yalc..."
+npm install -global yalc
+
+
+git clone https://github.com/multiversx/mx-sdk-dapp.git
+echo "cd mx-sdk-dapp..."
+cd mx-sdk-dapp
+git checkout development
+echo "Installing dependencies for mx-sdk-dapp..."
+yarn install
+echo "Building mx-sdk-dapp..."
+yarn build
+echo "Publishing mx-sdk-dapp..."
+cd dist
+yalc publish
+cd ../..
+
 
 git clone https://github.com/multiversx/mx-template-dapp.git
-
 echo "cd mx-template-dapp..."
 cd mx-template-dapp
 echo "git checkout main..."
 # TODO checkout main
 git checkout integration
-
-# Define the package name and the new version number
-PACKAGE_NAME="@multiversx/sdk-dapp"
-NEW_VERSION="multiversx/mx-sdk-dapp.git#development"
-
-# Use jq to update the sdk-dapp package version in package.json
-jq --arg new_version "$NEW_VERSION" '.dependencies |= . + {("'"$PACKAGE_NAME"'"): $new_version}' package.json > temp.json && mv temp.json package.json
-echo "$PACKAGE_NAME version has been updated to $NEW_VERSION in package.json"
-
-# Display the new version of the package.json file
-cat package.json
-
-echo "Installing dependencies for mx-template-dapp..."
+echo "Installing dependencies mx-template-dapp..."
 yarn install
+echo "Linking mx-sdk-dapp..."
+yalc link @multiversx/sdk-dapp
 echo "Building mx-template-dapp..."
 yarn build:devnet
 
